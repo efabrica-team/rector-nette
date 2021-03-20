0c7cab7544861f3768013560ac59d64421ec8b72
TomasVotruba <tomas.vot@gmail.com>
2021-03-20 22:06:03 +0100

    add KdybySetList

---
+++ config/sets/kdyby/kdyby-events-to-contributte-event-dispatcher.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Nette\Kdyby\Rector\ClassMethod\ChangeNetteEventNamesInGetSubscribedEventsRector;
+use Rector\Nette\Kdyby\Rector\ClassMethod\ReplaceMagicPropertyWithEventClassRector;
+use Rector\Nette\Kdyby\Rector\MethodCall\ReplaceEventManagerWithEventSubscriberRector;
+use Rector\Nette\Kdyby\Rector\MethodCall\ReplaceMagicPropertyEventWithEventClassRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(ChangeNetteEventNamesInGetSubscribedEventsRector::class);
+    $services->set(ReplaceMagicPropertyEventWithEventClassRector::class);
+    $services->set(ReplaceMagicPropertyWithEventClassRector::class);
+    $services->set(ReplaceEventManagerWithEventSubscriberRector::class);
+
+    $services->set(RenameClassRector::class)
+        ->call('configure', [[
+            RenameClassRector::OLD_TO_NEW_CLASSES => [
+                'Kdyby\Events\Subscriber' => 'Symfony\Component\EventDispatcher\EventSubscriberInterface',
+                'Kdyby\Events\EventManager' => 'Symfony\Contracts\EventDispatcher\EventDispatcherInterface',
+            ],
+        ]]);
+};
