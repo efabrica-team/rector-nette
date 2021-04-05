3e3b848c55bae61f73d93d2a2d867c7c74cf1db7
TomasVotruba <tomas.vot@gmail.com>
2021-04-05 15:45:25 +0200

    fix missing dir

--- config/config.php
+++ config/config.php
@@ -23,6 +23,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             __DIR__ . '/../src/ValueObject',
             __DIR__ . '/../src/Kdyby/Rector',
             __DIR__ . '/../src/Kdyby/ValueObject',
-            __DIR__ . '/../src/PhpDoc/Node',
         ]);
 };
