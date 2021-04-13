d9d0339d1b160ed6aaa54eaccfc5a1169b792c63
Tomas Votruba <tomas.vot@gmail.com>
2021-04-13 10:39:43 +0200

    Update to Rector 0.10.5 (#5)

--- tests/Kdyby/Rector/MethodCall/ReplaceEventManagerWithEventSubscriberRector/ReplaceEventManagerWithEventSubscriberRectorTest.php
+++ tests/Kdyby/Rector/MethodCall/ReplaceEventManagerWithEventSubscriberRector/ReplaceEventManagerWithEventSubscriberRectorTest.php
@@ -4,16 +4,37 @@ declare(strict_types=1);
 
 namespace Rector\Nette\Tests\Kdyby\Rector\MethodCall\ReplaceEventManagerWithEventSubscriberRector;
 
+use Iterator;
+use Rector\FileSystemRector\ValueObject\AddedFileWithContent;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\SmartFileSystem\SmartFileSystem;
 
 final class ReplaceEventManagerWithEventSubscriberRectorTest extends AbstractRectorTestCase
 {
-    public function test(): void
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo, AddedFileWithContent $expectedAddedFileWithContent): void
     {
-        $fixtureFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/fixture.php.inc');
         $this->doTestFileInfo($fixtureFileInfo);
-        $this->doTestExtraFile('Event/SomeClassCopyEvent.php', __DIR__ . '/Source/ExpectedSomeClassCopyEvent.php');
+        $this->assertFileWasAdded($expectedAddedFileWithContent);
+    }
+
+    /**
+     * @return Iterator<array<SmartFileInfo|AddedFileWithContent>>
+     */
+    public function provideData(): Iterator
+    {
+        $smartFileSystem = new SmartFileSystem();
+
+        yield [
+            new SmartFileInfo(__DIR__ . '/Fixture/fixture.php.inc'),
+            new AddedFileWithContent(
+                $this->getFixtureTempDirectory() . '/Event/SomeClassCopyEvent.php',
+                $smartFileSystem->readFile(__DIR__ . '/Source/ExpectedSomeClassCopyEvent.php')
+            ),
+        ];
     }
 
     public function provideConfigFilePath(): string
