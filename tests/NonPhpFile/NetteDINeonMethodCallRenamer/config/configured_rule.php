3350ed5778b6e71d5170e8be43e5971be31abad7
Michal Lulco <lulco@efabrica.sk>
2021-04-15 23:07:28 +0200

    Rewritten test

---
+++ tests/NonPhpFile/NetteDINeonMethodCallRenamer/config/configured_rule.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Nette\Tests\NonPhpFile\NetteDINeonMethodCallRenamer\Source\SecondService;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(RenameMethodRector::class)
+        ->call('configure', [[
+            RenameMethodRector::METHOD_CALL_RENAMES => ValueObjectInliner::inline([
+                new MethodCallRename(SecondService::class, 'add', 'addConfig'),
+            ]),
+        ]]);
+};
