69ea2b05552b5a29ffdc087d417d5bb768759537
TomasVotruba <tomas.vot@gmail.com>
2021-05-15 11:59:01 +0100

    add only for test, not globally to avoid breaking maing build

--- tests/Rector/ArrayDimFetch/ChangeFormArrayAccessToAnnotatedControlVariableRector/config/configured_rule.php
+++ tests/Rector/ArrayDimFetch/ChangeFormArrayAccessToAnnotatedControlVariableRector/config/configured_rule.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Rector\Nette\Rector\ArrayDimFetch\ChangeFormArrayAccessToAnnotatedControlVariableRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../../config/config.php');
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__ . '/nette-phpstan-for-rector.neon');
 
     $services = $containerConfigurator->services();
     $services->set(ChangeFormArrayAccessToAnnotatedControlVariableRector::class);
