5f495313207308ca07b577848db2ff06a47eca7d
Michal Lulco <lulco@efabrica.sk>
2021-08-16 23:39:05 +0200

    Rector for creating Template class and adding latte {templateType} based on presenter template parameters (#29)

---
+++ tests/Rector/Class_/TemplateTypeBasedOnPresenterTemplateParametersRector/TemplateTypeBasedOnPresenterTemplateParametersRectorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Nette\Tests\Rector\Class_\TemplateTypeBasedOnPresenterTemplateParametersRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @see \Rector\Nette\Rector\Class_\TemplateTypeBasedOnPresenterTemplateParametersRector
+ */
+final class TemplateTypeBasedOnPresenterTemplateParametersRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfoWithAdditionalChanges($fileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
