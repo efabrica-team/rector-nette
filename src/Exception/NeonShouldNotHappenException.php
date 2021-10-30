e05c5cd40b45fa2e7186935c28d1e16a34dce5df
Tomas Votruba <tomas.vot@gmail.com>
2021-10-30 13:15:31 +0200

    refactoring to neon ast

---
+++ src/Exception/NeonShouldNotHappenException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Nette\Exception;
+
+use Exception;
+
+final class NeonShouldNotHappenException extends Exception
+{
+}
--- src/Rector/Neon/RenameMethodNeonRector.php
+++ src/Rector/Neon/RenameMethodNeonRector.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace Rector\Nette\Rector\Neon;
 
+use Nette\Neon\Decoder;
+use Nette\Neon\Node;
+use Nette\Neon\Node\ArrayItemNode;
+use Nette\Neon\Node\ArrayNode;
+use Nette\Neon\Node\EntityNode;
+use Nette\Neon\Node\LiteralNode;
+use Nette\Neon\Traverser;
 use Nette\Utils\Strings;
 use Rector\Nette\Contract\Rector\NeonRectorInterface;
 use Rector\Renaming\Collector\MethodCallRenameCollector;
+use Rector\Renaming\Contract\MethodCallRenameInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -15,6 +23,11 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class RenameMethodNeonRector implements NeonRectorInterface
 {
+    /**
+     * @var string
+     */
+    private const SERVICES_KEYWORD = 'services';
+
     public function __construct(
         private MethodCallRenameCollector $methodCallRenameCollector
     ) {
@@ -45,20 +58,146 @@ CODE_SAMPLE
 
     public function changeContent(string $content): string
     {
-        foreach ($this->methodCallRenameCollector->getMethodCallRenames() as $methodCallRename) {
-            $oldObjectType = $methodCallRename->getOldObjectType();
-            $objectClassName = $oldObjectType->getClassName();
-            $className = str_replace('\\', '\\\\', $objectClassName);
+        $decoder = new Decoder();
+        $neonNode = $decoder->parseToNode($content);
 
-            $oldMethodName = $methodCallRename->getOldMethod();
-            $newMethodName = $methodCallRename->getNewMethod();
+        $isInServicesScope = false;
 
-            $pattern = '#\n(.*?)(class|factory): ' . $className . '(\n|\((.*?)\)\n)\1setup:(.*?)- ' . $oldMethodName . '\(#s';
-            if (Strings::match($content, $pattern)) {
-                $content = str_replace($oldMethodName . '(', $newMethodName . '(', $content);
+        $nodeVisitor = function (Node $neonNode) use (&$isInServicesScope) {
+            if ($isInServicesScope) {
+                if ($neonNode instanceof ArrayNode) {
+                    foreach ($neonNode->getSubNodes() as $serviceNode) {
+                        // iterate single service
+                        $serviceType = $this->resolveServiceType($serviceNode);
+
+                        // could not match the method name
+                        if (! is_string($serviceType)) {
+                            return null;
+                        }
+
+                        $methodCallRenames = $this->findMethodCallRenamesByClass($serviceType);
+                        if ($methodCallRenames === []) {
+                            return null;
+                        }
+
+                        $this->renameMethodCalls($methodCallRenames, $serviceNode);
+                    }
+                }
+            }
+
+            if ($neonNode instanceof LiteralNode) {
+                if ($neonNode->toValue() === self::SERVICES_KEYWORD) {
+                    $isInServicesScope = true;
+                }
+            }
+        };
+
+        // @see https://forum.nette.org/en/34804-neon-with-ast-parser-and-format-preserving-printing
+
+        $traverser = new Traverser();
+        $neonNode = $traverser->traverse($neonNode, $nodeVisitor);
+
+        $refactoredContent = $neonNode->toString();
+
+        // @todo how to preserve spaces?
+        $refactoredContent = Strings::replace($refactoredContent, '#\t#', '    ');
+
+        // replace quotes
+        return Strings::replace($refactoredContent, '#\"#', '\'');
+    }
+
+    private function resolveServiceType(Node $serviceNode): string|null
+    {
+        if (! $serviceNode instanceof ArrayItemNode) {
+            return null;
+        }
+
+        if (! $serviceNode->value instanceof ArrayNode) {
+            return null;
+        }
+
+        foreach ($serviceNode->value->items as $serviceConfigurationItem) {
+            if ($serviceConfigurationItem->key === null) {
+                continue;
+            }
+
+            if ($serviceConfigurationItem->key->toString() === 'factory') {
+                if ($serviceConfigurationItem->value instanceof EntityNode) {
+                    return $serviceConfigurationItem->value->value->toString();
+                }
+
+                return $serviceConfigurationItem->value->toString();
+            }
+
+            if ($serviceConfigurationItem->key->toString() === 'class') {
+                if ($serviceConfigurationItem->value instanceof EntityNode) {
+                    return $serviceConfigurationItem->value->value->toString();
+                }
+
+                return $serviceConfigurationItem->value->toString();
             }
         }
 
-        return $content;
+        return null;
+    }
+
+    /**
+     * @return MethodCallRenameInterface[]
+     */
+    private function findMethodCallRenamesByClass(string $class): array
+    {
+        $methodCallRenames = [];
+
+        foreach ($this->methodCallRenameCollector->getMethodCallRenames() as $methodCallRename) {
+            $objectType = $methodCallRename->getOldObjectType();
+            if ($objectType->getClassName() === $class) {
+                $methodCallRenames[] = $methodCallRename;
+            }
+        }
+
+        return $methodCallRenames;
+    }
+
+    /**
+     * @param MethodCallRenameInterface[] $methodCallRenames
+     */
+    private function renameMethodCalls(array $methodCallRenames, Node $serviceNode): Node|null
+    {
+        if (! $serviceNode instanceof ArrayItemNode) {
+            return null;
+        }
+
+        if (! $serviceNode->value instanceof ArrayNode) {
+            return null;
+        }
+
+        foreach ($serviceNode->value->items as $arrayItem) {
+            if ($arrayItem->key === null) {
+                continue;
+            }
+            // method calls
+            if ($arrayItem->key->toString() !== 'setup') {
+                continue;
+            }
+
+            if ($arrayItem->value instanceof ArrayNode) {
+                foreach ($arrayItem->value->items as $callArrayItem) {
+                    if ($callArrayItem->value instanceof EntityNode) {
+                        $methodName = $callArrayItem->value->value->toString();
+
+                        foreach ($methodCallRenames as $methodCallRename) {
+                            if ($methodCallRename->getOldMethod() !== $methodName) {
+                                continue;
+                            }
+
+                            // rename method happens here
+                            $callArrayItem->value->value = new LiteralNode($methodCallRename->getNewMethod());
+                        }
+                    }
+                }
+            }
+        }
+
+        return $serviceNode;
     }
 }
