b59d2f630aa226c17f1f621cc8516caacd505f39
Tomas Votruba <tomas.vot@gmail.com>
2021-11-23 15:22:24 +0300

    Revert "Allow nette/neon fixed master branch (#43)"
    This reverts commit cde297418876d4356c4a1a3894032cc051e23f5d.

--- packages/NeonParser/Node/AbstractVirtualNode.php
+++ packages/NeonParser/Node/AbstractVirtualNode.php
@@ -9,19 +9,13 @@ use Rector\Nette\NeonParser\Exception\UnusedVirtualMethodException;
 
 abstract class AbstractVirtualNode extends Node
 {
-    /**
-     * @param  callable(self): mixed|null  $evaluator
-     */
-    public function toValue(callable $evaluator = null): mixed
+    public function toValue(): mixed
     {
         // never used, just to make parent contract happy
         throw new UnusedVirtualMethodException();
     }
 
-    /**
-     * @param  callable(self): string|null  $serializer
-     */
-    public function toString(callable $serializer = null): string
+    public function toString(): string
     {
         // never used, just to make parent contract happy
         throw new UnusedVirtualMethodException();
