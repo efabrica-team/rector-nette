da717323dfd96861c481bff1fc06e80da0dfcfd0
Tomas Votruba <tomas.vot@gmail.com>
2021-11-30 19:55:12 +0300

    [DX] Make use of configure() method

--- config/sets/contributte/apitte-attributes.php
+++ config/sets/contributte/apitte-attributes.php
@@ -5,24 +5,21 @@ declare(strict_types=1);
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 // @see https://github.com/apitte/core/pull/161
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(AnnotationToAttributeRector::class)
-        ->call('configure', [[
-            AnnotationToAttributeRector::ANNOTATION_TO_ATTRIBUTE => ValueObjectInliner::inline([
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Id'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Method'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Negotiation'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\OpenApi'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Path'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\RequestBody'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\RequestParameter'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Response'),
-                new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Tag'),
-            ]),
-        ]]);
+        ->configure([
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Id'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Method'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Negotiation'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\OpenApi'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Path'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\RequestBody'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\RequestParameter'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Response'),
+            new AnnotationToAttribute('Apitte\Core\Annotation\Controller\Tag'),
+        ]);
 };
--- config/sets/kdyby/kdyby-translator-to-contributte-translation.php
+++ config/sets/kdyby/kdyby-translator-to-contributte-translation.php
@@ -7,26 +7,19 @@ use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(RenameMethodRector::class)
-        ->call('configure', [[
-            RenameMethodRector::METHOD_CALL_RENAMES => ValueObjectInliner::inline([
-                new MethodCallRename('Kdyby\Translation\Translator', 'translate', 'trans'),
-            ]),
-        ]]);
+        ->configure([new MethodCallRename('Kdyby\Translation\Translator', 'translate', 'trans')]);
 
     $services->set(RenameClassRector::class)
-        ->call('configure', [[
-            RenameClassRector::OLD_TO_NEW_CLASSES => [
-                'Kdyby\Translation\Translator' => 'Nette\Localization\ITranslator',
-                'Kdyby\Translation\DI\ITranslationProvider' => 'Contributte\Translation\DI\TranslationProviderInterface',
-                'Kdyby\Translation\Phrase' => 'Contributte\Translation\Wrappers\Message',
-            ],
-        ]]);
+        ->configure([
+            'Kdyby\Translation\Translator' => 'Nette\Localization\ITranslator',
+            'Kdyby\Translation\DI\ITranslationProvider' => 'Contributte\Translation\DI\TranslationProviderInterface',
+            'Kdyby\Translation\Phrase' => 'Contributte\Translation\Wrappers\Message',
+        ]);
 
     $services->set(WrapTransParameterNameRector::class);
 };
