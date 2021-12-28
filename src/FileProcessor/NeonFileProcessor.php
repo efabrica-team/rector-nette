bb2575d4df9cdb7d2dd32b1b21f41cbbb16bbe11
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-28 18:59:48 +0700

    [FileProcessor] Fix return array on FileProcessors (#53)
    * [FileProcessor] Fix return array on FileProcessors
    
    * use originalPrintedContent as the compared is originalPrintedContent
    
    * phpstan

--- src/FileProcessor/NeonFileProcessor.php
+++ src/FileProcessor/NeonFileProcessor.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace Rector\Nette\FileProcessor;
 
+use Rector\ChangesReporting\ValueObjectFactory\FileDiffFactory;
 use Rector\Core\Contract\Processor\FileProcessorInterface;
 use Rector\Core\ValueObject\Application\File;
 use Rector\Core\ValueObject\Configuration;
+use Rector\Core\ValueObject\Error\SystemError;
+use Rector\Core\ValueObject\Reporting\FileDiff;
 use Rector\Nette\Contract\Rector\NeonRectorInterface;
 use Rector\Nette\NeonParser\NeonNodeTraverserFactory;
 use Rector\Nette\NeonParser\NeonParser;
 use Rector\Nette\NeonParser\Printer\FormatPreservingNeonPrinter;
+use Rector\Parallel\ValueObject\Bridge;
 
 final class NeonFileProcessor implements FileProcessorInterface
 {
@@ -22,13 +26,22 @@ final class NeonFileProcessor implements FileProcessorInterface
         private NeonNodeTraverserFactory $neonNodeTraverserFactory,
         private FormatPreservingNeonPrinter $formatPreservingNeonPrinter,
         private array $neonRectors,
+        private FileDiffFactory $fileDiffFactory
     ) {
     }
 
-    public function process(File $file, Configuration $configuration): void
+    /**
+     * @return array{system_errors: SystemError[], file_diffs: FileDiff[]}
+     */
+    public function process(File $file, Configuration $configuration): array
     {
+        $systemErrorsAndFileDiffs = [
+            Bridge::SYSTEM_ERRORS => [],
+            Bridge::FILE_DIFFS => [],
+        ];
+
         if ($this->neonRectors === []) {
-            return;
+            return $systemErrorsAndFileDiffs;
         }
 
         $fileContent = $file->getFileContent();
@@ -47,10 +60,15 @@ final class NeonFileProcessor implements FileProcessorInterface
 
         // has node changed?
         if ($changedFileContent === $originalPrintedContent) {
-            return;
+            return $systemErrorsAndFileDiffs;
         }
 
         $file->changeFileContent($changedFileContent);
+
+        $fileDiff = $this->fileDiffFactory->createFileDiff($file, $originalPrintedContent, $changedFileContent);
+        $systemErrorsAndFileDiffs[Bridge::FILE_DIFFS][] = $fileDiff;
+
+        return $systemErrorsAndFileDiffs;
     }
 
     public function supports(File $file, Configuration $configuration): bool
