fd471e6b93fad0c6341cea71effee93673004d1f
Aleksey Polyvanyi <aleksey.polyvanyi@eonx.com>
2022-06-17 21:33:56 +0200

    -update configs

--- tests/Rector/Assign/MakeGetComponentAssignAnnotatedRector/config/auto_import.php
+++ tests/Rector/Assign/MakeGetComponentAssignAnnotatedRector/config/auto_import.php
@@ -9,8 +9,7 @@ use Rector\Nette\Rector\Assign\MakeGetComponentAssignAnnotatedRector;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
 
-    $parameters = $rectorConfig->parameters();
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $rectorConfig->importNames();
 
     $rectorConfig->rule(MakeGetComponentAssignAnnotatedRector::class);
 };
