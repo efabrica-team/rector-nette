3ea1ed73af509e160b099cd371618e70e2bdb704
Aleksey Polyvanyi <alexndlm@gmail.com>
2022-06-18 10:45:10 +0200

    [Nette] Update MakeGetComponentAssignAnnotatedRectorTest configs (#76)
    * -update configs
    
    * -cleanup

--- tests/Rector/Assign/MakeGetComponentAssignAnnotatedRector/config/auto_import.php
+++ tests/Rector/Assign/MakeGetComponentAssignAnnotatedRector/config/auto_import.php
@@ -3,14 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Nette\Rector\Assign\MakeGetComponentAssignAnnotatedRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
 
-    $parameters = $rectorConfig->parameters();
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $rectorConfig->importNames();
 
     $rectorConfig->rule(MakeGetComponentAssignAnnotatedRector::class);
 };
