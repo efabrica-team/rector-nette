30e8334c348423c31f2409dec59db677b316b5c3
GitHub Action <action@github.com>
2022-07-19 08:05:54 +0000

    [ci-review] Rector Rectify

--- src/Rector/ClassMethod/RemoveParentAndNameFromComponentConstructorRector.php
+++ src/Rector/ClassMethod/RemoveParentAndNameFromComponentConstructorRector.php
@@ -175,7 +175,7 @@ CODE_SAMPLE
             $parameterNames[] = $parameterReflection->getName();
         }
 
-        foreach ($new->args as $position => $arg) {
+        foreach (array_keys($new->args) as $position) {
             // is on position of $parent or $name?
             if (! isset($parameterNames[$position])) {
                 continue;
