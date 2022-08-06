0509d318cbadbe8d70157358d60a6418ed40c7b2
Tomas Votruba <tomas.vot@gmail.com>
2022-08-07 01:12:06 +0200

    Remove StartsWithFunctionToNetteUtilsStringsRector, as now we have str_starts_with() native function (#83)

--- config/sets/nette-utils-code-quality.php
+++ config/sets/nette-utils-code-quality.php
@@ -9,7 +9,6 @@ use Rector\Nette\Rector\FuncCall\PregFunctionToNetteUtilsStringsRector;
 use Rector\Nette\Rector\FuncCall\PregMatchFunctionToNetteUtilsStringsRector;
 use Rector\Nette\Rector\FuncCall\SubstrStrlenFunctionToNetteUtilsStringsRector;
 use Rector\Nette\Rector\Identical\EndsWithFunctionToNetteUtilsStringsRector;
-use Rector\Nette\Rector\Identical\StartsWithFunctionToNetteUtilsStringsRector;
 use Rector\Nette\Rector\LNumber\ReplaceTimeNumberWithDateTimeConstantRector;
 use Rector\Nette\Rector\NotIdentical\StrposToStringsContainsRector;
 use Rector\Transform\Rector\FuncCall\FuncCallToStaticCallRector;
@@ -25,7 +24,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->rule(StrposToStringsContainsRector::class);
     $rectorConfig->rule(SubstrStrlenFunctionToNetteUtilsStringsRector::class);
-    $rectorConfig->rule(StartsWithFunctionToNetteUtilsStringsRector::class);
     $rectorConfig->rule(PregMatchFunctionToNetteUtilsStringsRector::class);
     $rectorConfig->rule(PregFunctionToNetteUtilsStringsRector::class);
     $rectorConfig->rule(EndsWithFunctionToNetteUtilsStringsRector::class);
